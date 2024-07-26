405149cad1b0aca7fb81bdbc7c766575434bfab0
Phamducvinh <phamducvinh2k3@gmail.com>
2024-07-26 10:01:09 +0700

    updatev5

--- server1.js
+++ server1.js
@@ -19,16 +19,33 @@ if (!fs.existsSync(uploadsDir)) {
 }
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  tls: true,
-  tlsAllowInvalidCertificates: true,
-}).catch(error => console.error('Error connecting to MongoDB:', error));
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://vinhpham981:Vinhpham981@cluster0.brwtjha.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // Define Schema and Model
 const imageSchema = new mongoose.Schema({
